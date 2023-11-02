3f8c7fade8807492bea1d2ee8613e45c677eb1fc
BenDXC <benlate0@gmail.com>
2023-11-02 17:35:10 +0000

    1.0 Initalise Environment var of MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.listen(8900, () => {
+  connect();
   console.log("Connected Backend");
 });
