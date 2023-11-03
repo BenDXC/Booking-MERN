0f0dd080bea49bb60b452e055343c46793a6f8fa
BenDXC <benlate0@gmail.com>
2023-11-03 15:21:31 +0000

    1.0 Connection to MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -13,6 +20,31 @@ const connect = async () => {
     throw error;
   }
 };
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+/* Middlewares */
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/Backend/auth", authRoute);
+app.use("/Backend/users", usersRoute);
+app.use("/Backend/hotels", hotelsRoute);
+app.use("/Backend/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 app.listen(8900, () => {
   connect();
